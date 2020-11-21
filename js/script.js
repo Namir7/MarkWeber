70825ba340a6b383079d00e9a57c9f26db3c8ccb
albertthewinner2000 <nalbert2012@ya.ru>
2020-11-21 08:13:18 +0300

    feat: added categories btn and slider
    added funcion for serve categories btns and slider-like element

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ slider3();
 
 variety();
 
+catalog();
 //sliders
 function slider1() {
   //slider1
@@ -91,7 +92,7 @@ function slider1() {
     slider1Track.addEventListener("mouseout", () => {
       timerId = setInterval(() => {
         nextBtn1.click();
-      }, 1500);
+      }, 3000);
     });
   }
 }
@@ -262,65 +263,234 @@ function variety() {
 }
 
 //catalog
+function catalog() {
+  //    categories constants
+  const catalog = document.querySelector(".catalog");
+  const categoriesBtns = catalog.querySelectorAll("#categoriesBtn");
 
-//categories
-// window.addEventListener("keydown", (event) => {
-//   console.log(event);
-// });
+  //    blocks constatns
+  const catalogNextBtn = catalog.querySelector("#catalogNextBtn");
+  const catalogPrevBtn = catalog.querySelector("#catalogPrevBtn");
 
-const catalog = document.querySelector(".catalog");
-const categoriesBtns = catalog.querySelectorAll("#categoriesBtn");
-
-//  disable TAB keydown for unnecessary elements
-{
-  const disableTABKeydownElements = document.querySelectorAll(
-    "button:not(#categoriesBtn), a, select, input"
+  const catalogCategoriesBlocks = catalog.querySelectorAll(
+    ".catalog__main-category-content"
   );
 
-  Array.from(disableTABKeydownElements).forEach((disableTABKeydownElement) => {
-    disableTABKeydownElement.setAttribute("tabindex", -1);
+  const categoryContentBlocks = catalog.querySelectorAll(
+    ".catalog__main-category-content-block"
+  );
+  //disable
+  //    TAB
+  disableTAB();
+  // empty categories
+  disableEmpty()
+
+  //  listeners and events
+
+  const changeActive = new CustomEvent("changeActive", {
+    detail: {
+      prev: null,
+      current: categoriesBtns[0],
+    },
   });
-}
 
-//  change catalog
-// counter for tabIndex
-let tabIndexCounter = 0;
-// categoriesBtns[0].setAttribute("data-active", 'active');
-categoriesBtns[1].setAttribute("tabindex", 1);
+  catalog.addEventListener("changeActive", (event) => {
+    console.log(`catalog Listener: active category changed`);
+    smoothScroll();
 
+    setAndRemoveActiveAttributeToIcons();
 
-Array.from(categoriesBtns).forEach((categoriesBtn) => {
-  categoriesBtn.addEventListener(
-    "focus",
-    (event) => {
-      //scroll window
+    changeCategoryContent();
+
+    changeBlockContent();
+
+    dispatchActiveCategoryBlockEvent();
+
+    function smoothScroll() {
       window.scroll({
         top: 520,
         // top: 790,
         left: 0,
         behavior: "smooth",
       });
+    }
 
-      //infinitive TAB
-      tabIndexCounter++;
-      event.target.setAttribute("data-active", 'active');
+    function setAndRemoveActiveAttributeToIcons() {
+      event.detail.prev.removeAttribute("data-active");
+      event.detail.current.setAttribute("data-active", "active");
+    }
 
-      if (tabIndexCounter % categoriesBtns.length === 0) {
-        categoriesBtns[0].setAttribute("tabindex", tabIndexCounter);
-      } else {
-        event.target.nextElementSibling.setAttribute(
-          "tabindex",
-          tabIndexCounter
-        );
-      }
+    function changeCategoryContent() {
+      let previousCategoryNumber = event.detail.prev.getAttribute(
+        "data-categoryNumber"
+      );
 
-      
-      console.log(event.target);
-    },
-    false
+      catalogCategoriesBlocks[previousCategoryNumber - 1].removeAttribute(
+        "data-active"
+      );
+
+      let currentCategoryNumber = event.detail.current.getAttribute(
+        "data-categoryNumber"
+      );
+
+      catalogCategoriesBlocks[currentCategoryNumber - 1].setAttribute(
+        "data-active",
+        "active"
+      );
+    }
+
+    function changeBlockContent() {
+      //  current
+      let currentCategoryNumber = event.detail.current.getAttribute(
+        "data-categoryNumber"
+      );
+      let currentBlockNumber = 1;
+      let currentBlock = catalogCategoriesBlocks[
+        currentCategoryNumber - 1
+      ].querySelector(
+        `.catalog__main-category-content-block[data-blockNumber='${currentBlockNumber}']`
+      );
+
+      currentBlock.setAttribute("data-active", "active");
+      // previous
+      let previousCategoryNumber = event.detail.prev.getAttribute(
+        "data-categoryNumber"
+      );
+      let previousBlockNumber = changeActiveCategoryBlock.detail.currentBlock.getAttribute(
+        "data-blockNumber"
+      );
+      let previousBlock = catalogCategoriesBlocks[
+        previousCategoryNumber - 1
+      ].querySelector(
+        `.catalog__main-category-content-block[data-blockNumber='${previousBlockNumber}']`
+      );
+
+      previousBlock.removeAttribute("data-active");
+    }
+
+    function dispatchActiveCategoryBlockEvent() {
+      let currentCategoryNumber = event.detail.current.getAttribute(
+        "data-categoryNumber"
+      );
+      let currentBlock = catalogCategoriesBlocks[
+        currentCategoryNumber - 1
+      ].querySelector(
+        `.catalog__main-category-content-block[data-blockNumber='1']`
+      );
+
+      changeActiveCategoryBlock.detail.currentBlock = currentBlock;
+      changeActiveCategoryBlock.detail.action = null;
+
+      catalog.dispatchEvent(changeActiveCategoryBlock);
+    }
+  });
+
+  const changeActiveCategoryBlock = new CustomEvent(
+    "changeActiveCategoryBlock",
+    {
+      detail: {
+        currentBlock: categoryContentBlocks[0],
+        action: null,
+      },
+    }
   );
-});
 
-//useful commemts
+  catalog.addEventListener("changeActiveCategoryBlock", (event) => {
+    console.log(`catalog Listener: active block changed`);
 
-// window.scrollBy(0, event.detail.current.nextElementSibling.clientHeight);
+    //    display current block
+    // hide prev block
+
+    if (event.detail.action === "next")
+      event.detail.currentBlock.previousElementSibling.removeAttribute(
+        "data-active"
+      );
+
+    if (event.detail.action === "prev")
+      event.detail.currentBlock.nextElementSibling.removeAttribute(
+        "data-active"
+      );
+
+    //    show current block
+    event.detail.currentBlock.setAttribute("data-active", "active");
+
+    //    disable
+    if (
+      Number.parseInt(
+        event.detail.currentBlock.getAttribute("data-blockNumber")
+      ) === 1
+    ) {
+      catalogPrevBtn.setAttribute("disabled", "disabled");
+    } else {
+      catalogPrevBtn.removeAttribute("disabled");
+    }
+    if (
+      Number.parseInt(
+        event.detail.currentBlock.getAttribute("data-blockNumber")
+      ) === 3
+    ) {
+      catalogNextBtn.setAttribute("disabled", "disabled");
+    } else {
+      catalogNextBtn.removeAttribute("disabled");
+    }
+    //  catalog content
+  });
+
+  //        buttons listeners
+
+  Array.from(categoriesBtns).forEach((categoriesBtn) => {
+    //when focus
+    categoriesBtn.addEventListener("focus", (event) => {
+      changeActive.detail.prev = changeActive.detail.current;
+      changeActive.detail.current = event.target;
+
+      catalog.dispatchEvent(changeActive);
+    });
+  });
+
+  catalogNextBtn.addEventListener("click", (event) => {
+    changeActiveCategoryBlock.detail.action = "next";
+    changeActiveCategoryBlock.detail.currentBlock =
+      changeActiveCategoryBlock.detail.currentBlock.nextElementSibling;
+
+    catalog.dispatchEvent(changeActiveCategoryBlock);
+    // console.log(changeActiveCategoryBlock.detail.currentBlock);
+  });
+
+  catalogPrevBtn.addEventListener("click", (event) => {
+    changeActiveCategoryBlock.detail.action = "prev";
+    changeActiveCategoryBlock.detail.currentBlock =
+      changeActiveCategoryBlock.detail.currentBlock.previousElementSibling;
+
+    catalog.dispatchEvent(changeActiveCategoryBlock);
+    // console.log(changeActiveCategoryBlock.detail.currentBlock);
+  });
+
+  //initial changes
+  document.addEventListener("DOMContentLoaded", () => {
+    categoriesBtns[0].focus();
+  });
+
+  //    functions
+  //      disables TAB for unessessary elements
+  function disableTAB() {
+    const disableTABKeydownElements = document.querySelectorAll(
+      "button:not(#categoriesBtn), a, select, input"
+    );
+
+    Array.from(disableTABKeydownElements).forEach(
+      (disableTABKeydownElement) => {
+        disableTABKeydownElement.setAttribute("tabindex", -1);
+      }
+    );
+  }
+
+  function disableEmpty() {
+    Array.from(categoriesBtns).forEach((categoriesBtn, categoriesBtnIndex) => {
+      let categoryElement = catalog.querySelector(`.catalog__main-category-content[data-categoryNumber='${categoriesBtnIndex + 1}']`); 
+      if (categoryElement.children.length === 0) categoriesBtn.setAttribute('disabled', 'disabled');
+      console.log(categoriesBtn);
+    });
+  }
+
+}
