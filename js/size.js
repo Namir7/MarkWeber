892f946f84165f327b3805d77cca09c71586d8a1
albertthewinner2000 <nalbert2012@ya.ru>
2020-11-23 09:57:02 +0300

    feat:added respoonsive for pre-last section

--- js/size.js
+++ js/size.js
@@ -1,3 +1,13 @@
+// window.addEventListener('DOMContentLoaded', ()=>{
+//   console.log(`Initial innerWidth1: `);
+//   console.log(window.innerWidth);
+// })
+
+// window.addEventListener('load', ()=>{
+//   console.log(`Initial innerWidth2:   `);
+//   console.log(window.innerWidth);
+// })
+
 window.addEventListener("click", () => {
     console.log(`innerWidth:`);
     console.log(window.innerWidth);
